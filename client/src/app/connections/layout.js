69134862b1afd4305ae90f4ec7c982b3f4da07a6
MOHAMMAD HANZALA KHAN <hanzalakhan409@gmail.com>
2023-08-16 03:48:34 +0530

    metadata fixed

--- client/src/app/connections/layout.js
+++ client/src/app/connections/layout.js
@@ -1,15 +1,6 @@
 import Navbar from '@/components/Navbar'
 import SideMenu from '@/components/SideMenu'
-import { Outfit } from 'next/font/google'
 
-const OutfitFont = Outfit({
-  subsets: ['latin']
-})
-
-export const metadata = {
-  title: 'Moilicis Assigment',
-  description: 'Created by Mohammad Hanzala Khan',
-}
 
 export default function RootLayout({ children }) {
   return (
--- client/src/app/myprofile/layout.js
+++ client/src/app/myprofile/layout.js
@@ -1,18 +1,8 @@
 "use client"
 import Navbar from '@/components/Navbar'
 import SideMenu from '@/components/SideMenu'
-import { Outfit } from 'next/font/google'
 
 
-
-const OutfitFont = Outfit({
-  subsets: ['latin']
-})
-
-export const metadata = {
-  title: 'Moilicis Assigment',
-  description: 'Created by Mohammad Hanzala Khan',
-}
 
 export default function RootLayout({ children }) {
   return (
