5eb0a70c9617d312ede622605c7afa02a5de8e05
MOHAMMAD HANZALA KHAN <hanzalakhan409@gmail.com>
2023-08-16 14:52:29 +0530

    index3

--- server/index.js
+++ server/index.js
@@ -23,21 +23,15 @@ app.use(cors());
 
 // app.options('*', cors());  // Enable preflight requests for all routes
 
-app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, x-auth-token');
-    res.sendStatus(200);
-  });
+// app.options('*', (req, res) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, x-auth-token');
+//     res.sendStatus(200);
+//   });
 
 
 app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    next();
-});
 
 
 
@@ -45,11 +39,7 @@ app.use((req, res, next) => {
 app.use('/auth', usersRoutes); 
 app.use('/user', userProfileRoutes); 
 
-// Error handling middleware
-app.use((error, req, res, next) => {
-    console.error(error.stack);
-    res.status(500).send('Something went wrong!');
-});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
