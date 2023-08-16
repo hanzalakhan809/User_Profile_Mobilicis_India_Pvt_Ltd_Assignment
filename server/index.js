abf769e233d2a3700d6c4a1fa61290e17dd50c59
MOHAMMAD HANZALA KHAN <hanzalakhan409@gmail.com>
2023-08-16 13:48:55 +0530

    index.js updated

--- server/index.js
+++ server/index.js
@@ -6,32 +6,31 @@ require('dotenv').config();
 const usersRoutes = require('./routes/users');
 const userProfileRoutes = require('./routes/userProfile');
 
-//CONNECTION TO MONDODB ATLAS
+// CONNECTION TO MONDODB ATLAS
 const URI = process.env.MONGO_URI;
 mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(
-    console.log("Database Connected")
-)
-
-
+}).then(() => {
+    console.log("Database Connected");
+});
 
 const app = express();
 
-//MIDDLEWARES
+// MIDDLEWARES
 
 const corsOptions = {
     origin: 'https://user-profile-mobilicis-india-pvt-ltd-assignment-wsiz.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'x-auth-token'],
     credentials: true,
-    optionsSuccessStatus: 200 // For legacy browser support
+    optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));  // Enable preflight requests for all routes
 
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
@@ -39,31 +38,23 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.use(cors({
-//     origin: ["https://user-profile-mobilicis-india-pvt-ltd-assignment-wsiz.vercel.app"],
-//     methods: ["POST","GET","PUT"],
-//     credentials: true
-// }));
-// app.use(bodyParser.json());
-
-
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-})
-
-
-
-
+// Logging middleware for debugging
+app.use((req, res, next) => {
+    console.log(`Received ${req.method} request on ${req.path}`);
+    next();
+});
 
 // APIs
-
 app.use('/auth', usersRoutes); 
-
 app.use('/user', userProfileRoutes); 
 
+// Error handling middleware
+app.use((error, req, res, next) => {
+    console.error(error.stack);
+    res.status(500).send('Something went wrong!');
+});
 
-
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
